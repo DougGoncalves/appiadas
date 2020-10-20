2937fa72788d8cfa0d0108d7f1b0dcf16f601a67
DougGoncalves <doug.goncalvesf@gmail.com>
2020-10-19 22:57:04 -0300

    First Commit

---
+++ app/src/main/java/br/com/douggoncalves/appiadas/MainActivity.kt
@@ -0,0 +1,32 @@
+package br.com.douggoncalves.appiadas
+
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showJoker()
+
+        btTellJoker.setOnClickListener {
+            showJoker()
+        }
+    }
+    fun showJoker() {
+        val jokers = resources.getStringArray(R.array.jokers)
+        val numberJoker = Random().nextInt(jokers.size)
+        tvJoker.text = jokers[numberJoker]
+        playSong()
+    }
+
+    private fun playSong() {
+        val mediaPlayer = MediaPlayer.create(this, R.raw.badumtss)
+        mediaPlayer.start()
+    }
+
+}
